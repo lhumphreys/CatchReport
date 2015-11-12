0ea5d9a10f3df05769a166a72398140b81aa6130
lhumphreys <lhumphreys@eagles.ewu.edu>
2015-11-11 18:44:42 -0800

    Launch activity added

--- app/src/main/java/com/app/fish/catchreport/AddFishActivity.java
+++ app/src/main/java/com/app/fish/catchreport/AddFishActivity.java
@@ -1,11 +1,17 @@
 package com.app.fish.catchreport;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class AddFishActivity extends AppCompatActivity {
 
@@ -16,12 +22,33 @@ public class AddFishActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Spinner speciesSpin = (Spinner) findViewById(R.id.speciesSpinner);
+        ArrayList<String> species = new ArrayList<String>();
+        species.add("Trout");
+        species.add("Salmon");
+        species.add("Goldfish");
+        species.add("Great White");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, species);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        speciesSpin.setAdapter(adapter);
+
+        Button addButton = (Button) findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), AddFishActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/app/fish/catchreport/MainActivity.java
+++ app/src/main/java/com/app/fish/catchreport/MainActivity.java
@@ -18,23 +18,14 @@ public class MainActivity extends AppCompatActivity{
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-/*
-        Button addFishButton = (Button) findViewById(R.id.addButton);
+        Button addFishButton = (Button) findViewById(R.id.addFishButton);
         addFishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), AddFishActivity.class);
                 startActivity(intent);
             }
-        });*/
+        });
     }
 
 
