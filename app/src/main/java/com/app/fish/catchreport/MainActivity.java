72d8cef2e9dec70bd1bf783d9b864012455cbf32
lhumphreys <lhumphreys@eagles.ewu.edu>
2015-11-11 18:22:02 -0800

    AddFish page added

---
+++ app/src/main/java/com/app/fish/catchreport/MainActivity.java
@@ -0,0 +1,42 @@
+package com.app.fish.catchreport;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+/*
+        Button addFishButton = (Button) findViewById(R.id.addButton);
+        addFishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), AddFishActivity.class);
+                startActivity(intent);
+            }
+        });*/
+    }
+
+
+
+}
