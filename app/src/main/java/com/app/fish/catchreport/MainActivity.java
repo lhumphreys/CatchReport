8bedf35cd65ae77f9a5596f59b844866c62cd9ae
WillyPluto <sherwoodwt@gmail.com>
2015-11-21 18:47:34 -0800

    Prototype with database hookup and trip info

--- app/src/main/java/com/app/fish/catchreport/MainActivity.java
+++ app/src/main/java/com/app/fish/catchreport/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity{
         addFishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), AddFishActivity.class);
+                Intent intent = new Intent(v.getContext(), TripInfoPage.class);
                 startActivity(intent);
             }
         });
